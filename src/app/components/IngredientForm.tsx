b2c50dbdf0c2748e6afe6b4be2c75d62a680352e
Cody Burchell <cody.burchell@gmail.com>
2023-09-29 14:20:49 -0500

    Continuing progress on ingredients form

--- src/app/components/IngredientForm.tsx
+++ src/app/components/IngredientForm.tsx
@@ -2,17 +2,36 @@
 
 import React from "react";
 import { Ingredient } from "@prisma/client";
+import { useState, useRef } from "react";
 
 export default function IngredientForm(props: { ingredients: Ingredient[] }) {
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
+  const ingredientsSelectRef = useRef(null);
+
+  function selectHandler() {
+    const selectedIngredient = props.ingredients.find(function(ingredient) {
+      return ingredient.id.toString() == ingredientsSelectRef.current.value
+    })
+
+    if (selectedIngredient) {
+      setSelectedIngredients([...selectedIngredients, selectedIngredient])
+    }
+  }
 
   return(
+    <div>
+      <label htmlFor="ingredients">Ingredients</label>
+      <select defaultValue="default" id="ingredients" onChange={selectHandler} ref={ingredientsSelectRef}>
+        <option value="default" disabled>Select an ingredient</option>
+        {props.ingredients.map((ingredient) => (
+          <option key={ingredient.id} value={ingredient.id}>{ingredient.name}</option>
+        ))}
+      </select>
       <div>
-        <label htmlFor="ingredients">Ingredients</label>
-        <select id="ingredients">
-          {props.ingredients.map((ingredient) => (
-            <option value={ingredient.id}>{ingredient.name}</option>
-          ))}
-        </select>
+        {selectedIngredients.map((ingredient) => (
+          <p>{ingredient.name}</p>
+        ))}
       </div>
+    </div>
   )
 }
